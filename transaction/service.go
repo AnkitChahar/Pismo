12fda1ddb890ea9d6016635b5ca34c2346577f84
ankit <ankitchahar11@gmail.com>
2024-11-09 18:02:02 +0530

    enhancements

--- transaction/service.go
+++ transaction/service.go
@@ -29,15 +29,17 @@ var _ TransactionService = (*Service)(nil)
 func (s *Service) CreateTransaction(transaction *Transaction) (*Transaction, error) {
 	accountId := transaction.AccountID
 
+	// Validate if account exists with the given accountId
 	_, errAccount := s.accountSvc.GetAccountByID(accountId)
 	if errAccount != nil {
 		return nil, fmt.Errorf("error while getting account: %w", errAccount)
 	}
 
 	transaction.EventDate = time.Now()
-	if errCreate := s.db.Create(transaction).Error; errCreate != nil {
-		return nil, errCreate
+	res := s.db.Create(transaction)
+	if res.Error != nil {
+		return nil, res.Error
 	}
 
-	return transaction, nil
+	return res.Statement.Model.(*Transaction), nil
 }
